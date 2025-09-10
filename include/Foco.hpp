90597e63de1490f7365af3bfed1df85a39bfcfc8
Dalv88 <a24310347@live.ceti.mx>
2025-09-10 02:25:35 +0000

    practica 1

---
+++ include/Foco.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <EstadoFoco.hpp>
+
+
+class Foco
+{
+private:
+    EstadoFoco estado;
+
+public:
+    Foco() {}
+    ~Foco() {}
+    void Encender() { estado = true; }
+    void Apagar() { estado = false; }
+    EstadoFoco LeerEstado() { return estado; }
+};
